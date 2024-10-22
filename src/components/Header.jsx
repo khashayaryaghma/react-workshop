96935446f29b9b3485849c09490db295cc79e1ec
khashayar yaghma <khashayaryaghma@gmail.com>
2024-10-22 14:00:22 +0330

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,29 @@
-
+import { Link } from "react-router-dom";
+import "./header.css";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <header>
+      <Link to="/">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDjcHUJmbeEjlTGBf5sKqNm4c5JPF6CHoD6Q&s" alt="Logo" className="logo" />
+      </Link>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/" className="navLink">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" className="navLink">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+
+    </header>
+  );
 }
 
-export default Header
+export default Header;
