a541dffa3e14675d2277a091db5532a9d1aa3697
khashayar yaghma <khashayaryaghma@gmail.com>
2024-10-22 15:39:00 +0330

    fix card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,20 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./card.css";
+import { faArrowCircleRight } from "@fortawesome/free-solid-svg-icons";
 
 function Card({ item }) {
   return (
     <article>
-      <span className="rank">{item.rank}</span>
-      <h4>
-        <span className="symbol">{item.symbol}</span> | <span>{item.name}</span>
-      </h4>
+      <div className="box1">
+        <span className="rank">{item.rank}</span>
+        <h4>
+          <span className="symbol">{item.symbol}</span> | <span>{item.name}</span>
+        </h4>
+        <h4>${parseFloat(item.priceUsd).toFixed(2)}</h4>
+      </div>
+      <div className="box2">
+        <FontAwesomeIcon icon={faArrowCircleRight} size="2xl" />
+      </div>
     </article>
   );
 }
