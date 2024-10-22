139906ee6269ce4b1f26f8d486f57b91f7d009a2
khashayar yaghma <khashayaryaghma@gmail.com>
2024-10-22 13:24:37 +0330

    add font footer ...

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,15 @@
-
-
+import { Link } from "react-router-dom";
+import "./footer.css"
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer>
+      <h2>More projects I’ve worked on:</h2>
+      <Link to="https://github.com/khashayaryaghma" target="_blank" className="link">
+        {/* <GitHubIcon fontSize="large" /> */}
+        <p>@khashayaryaghma on github</p>
+      </Link>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
