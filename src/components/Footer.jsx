6a2983948608b1c4099fa43bbf35d523500d4674
khashayar yaghma <khashayaryaghma@gmail.com>
2024-10-22 13:34:10 +0330

    complete footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
-import "./footer.css"
+import "./footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 function Footer() {
   return (
     <footer>
-      <h2>More projects I’ve worked on:</h2>
+      <h3>More projects I’ve worked on:</h3>
       <Link to="https://github.com/khashayaryaghma" target="_blank" className="link">
-        {/* <GitHubIcon fontSize="large" /> */}
+        <FontAwesomeIcon icon={faGithub} size="2xl" />
         <p>@khashayaryaghma on github</p>
       </Link>
     </footer>
